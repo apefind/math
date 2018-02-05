f63464e70c5ddbf26be3c45a707773a945a59b1c
APEFind <apefind@gmail.com>
2018-02-05 18:46:13 +0100

    ...

--- golden_section.py
+++ golden_section.py
@@ -1,4 +1,5 @@
-"""Basic implementation of the golden section search
+"""Basic implementation of the golden section search, this easily can be improved 
+by storing and resuing the results of the previous iteration
 """
 
 import math
@@ -6,7 +7,7 @@ import math
 
 def golden_section_search(f, I, eps=0.00001):
     t = 0.5 * (math.sqrt(5) - 1)
-    a, b = I
+    a, b = Iring
     while abs(b - a) > eps:
         x, y = b - t * (b - a), a + t * (b - a)
         if f(x) > f(y):
