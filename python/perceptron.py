b8c8d351142d924130fdd74ae3fbc194b91d02b6
APEFind <apefind@gmail.com>
2019-02-20 11:48:28 -0800

    code subdir

---
+++ python/perceptron.py
@@ -0,0 +1,19 @@
+
+def and_(x, y):
+    return int(x and y)
+
+
+def or_(x, y):
+    return int(x or y)
+
+
+def perceptron(f, M, t=0.1):
+    w, b = [], 0
+    return w, b
+
+
+if __name__ == '__main__':
+    f = lambda x: (x + p) ** 2 + q
+    w, b = perceptron(and_, I)
+    print(f'{w}: {b}')
+    
