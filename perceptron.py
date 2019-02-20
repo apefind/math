2c1cdb9867a5931d67625c270349b55cfe64caf2
APEFind <apefind@gmail.com>
2019-02-20 10:45:13 -0800

    fixes

--- perceptron.py
+++ perceptron.py
@@ -6,13 +6,12 @@ def and_(a, b):
     return int(a and b)
 
 
-def perceptron(f, n, eps=0.00001):
+def perceptron(f, n, t=0.00001):
     pass
 
 
 if __name__ == '__main__':
-    p, q, I = 0, 0, (-10, 10)
-    p, q, I = -4, 1, (-10, 10)
     f = lambda x: (x + p) ** 2 + q
-    x0 = golden_section_search(f, I)
+    x0 = perceptron(f, I)
     print(f'arg min f on {I}: {x0}')
+    
