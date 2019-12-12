ca00601b19bb8fbcdb0acf46f399f78f08be10a0
APEFind <apefind@gmail.com>
2019-12-12 08:37:14 +0100

    Using notebooks

---
+++ notebook/boolean.py
@@ -0,0 +1,19 @@
+def AND(x, y):
+    return int(x and y)
+
+
+def OR(x, y):
+    return int(x or y)
+
+
+def NAND(x, y):
+    return int(not x and not y)
+
+
+def XOR(x, y):
+    return int(x and not y or not x and y)
+
+
+BOOLEAN_OPERATORS = AND, OR, NAND, XOR
+BOOLEAN_OPERATOR_DOMAIN = (0, 0), (0, 1), (1, 0), (1, 1)
+BOOLEAN_TESTDATA = [(f, [(x, f(*x)) for x in BOOLEAN_OPERATOR_DOMAIN]) for f in BOOLEAN_OPERATORS]
