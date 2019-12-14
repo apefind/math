d1bee899e323f3b961f924340770e5dd1cf737d0
APEFind <apefind@gmail.com>
2019-12-14 16:30:46 +0100

    ...

--- notebook/delta_rule.py
+++ notebook/delta_rule.py
@@ -2,18 +2,14 @@ from random import random
 from vector import dotprod
 
 
-def delta_rule(T, a, da, t=0.01, eps=0.05, epochs=50):
+def delta_rule(T, a, da, t=0.01, epochs=50):
     n = len(T[0])
     w, b = n * (random(),), random()
     for _ in range(epochs):
-        err = 0.0
         for x, y in T:
             z = dotprod(w, x) + b
             az, daz = a(z), da(z)
             d = y - az
             w = tuple(w[i] + t * d * daz * x[i] for i in range(n))
             b = b + t * d * daz
-            err = err + d * d
-        if 0.5 * err < eps:
-            break
     return w, b
